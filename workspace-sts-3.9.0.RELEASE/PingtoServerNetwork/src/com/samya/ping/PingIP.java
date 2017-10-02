d81f8ceeb02927f3f6b2530ce65a7ebc22333307
SG05477 <SG05477@denb0271.de.corp.danet.com>
2017-10-02 11:26:33 +0200

    First commit for ping ip

---
+++ workspace-sts-3.9.0.RELEASE/PingtoServerNetwork/src/com/samya/ping/PingIP.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package com.samya.ping;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * @author Samya Ghosh 
+ *
+ */
+public class PingIP {
+	
+	public static void runSystemCommand(String command) {
+
+		try {
+			Process p = Runtime.getRuntime().exec(command);
+			BufferedReader inputStream = new BufferedReader(
+					new InputStreamReader(p.getInputStream()));
+
+			String s = "";
+			// reading output stream of the command
+			while ((s = inputStream.readLine()) != null) {
+				System.out.println(s);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		String ip = "127.0.0.1";
+		runSystemCommand("ping " + ip);
+
+	
+	}
+
+}
